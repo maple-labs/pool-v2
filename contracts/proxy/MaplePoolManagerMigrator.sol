e7433fce6fd22ebfb04007c40f6a893f865a4b1d
Vedran Bidin <vedran@bidin.me>
2023-09-21 19:28:20 +0300

    feat: Add `PoolPermissionManager` migrator (#285)
    * feat: add ppm migrator
    
    * test: add tests for migrator
    
    * test: add storage slot assertion
    
    * fix: address pr comments
    
    * chore: formatting
    
    ---------
    
    Co-authored-by: 0xfarhaan <59924029+0xfarhaan@users.noreply.github.com>

---
+++ contracts/proxy/MaplePoolManagerMigrator.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.7;
+
+import { MapleProxiedInternals } from "../../modules/maple-proxy-factory/contracts/MapleProxiedInternals.sol";
+
+import { IMapleProxyFactoryLike, IGlobalsLike } from "../interfaces/Interfaces.sol";
+
+import { MaplePoolManagerStorage } from "./MaplePoolManagerStorage.sol";
+
+contract MaplePoolManagerMigrator is MapleProxiedInternals, MaplePoolManagerStorage {
+
+    event PoolPermissionManagerSet(address poolPermissionManager_);
+
+    fallback() external {
+        address poolPermissionManager_ = abi.decode(msg.data, (address));
+        address globals_               = IMapleProxyFactoryLike(_factory()).mapleGlobals();
+
+        require(IGlobalsLike(globals_).isInstanceOf("POOL_PERMISSION_MANAGER", poolPermissionManager_), "PMM:INVALID_PPM");
+
+        poolPermissionManager = poolPermissionManager_;
+
+        emit PoolPermissionManagerSet(poolPermissionManager_);
+    }
+
+}
